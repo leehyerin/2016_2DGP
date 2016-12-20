bf4ac2e1e491ca2adb4c48383d24140ed8b6d616
leehyerin <lhr_id@naver.com>
2016-12-21 04:05:41 +0900

    Skill button

--- Skill.py
+++ Skill.py
@@ -1,1 +1,50 @@
-class Skill:
+from pico2d import *
+
+
+class Skill__W:
+    def __init__(self):
+        self.x, self.y = 100, 550
+        self.image = load_image('resource/skill/skill_water.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Skill__L:
+    def __init__(self):
+        self.x, self.y = 140, 550
+        self.image = load_image('resource/skill/skill_lightning.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Skill__F:
+    def __init__(self):
+        self.x, self.y = 180, 550
+        self.image = load_image('resource/skill/skill_water.png')
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Skill__E:
+    def __init__(self):
+        self.x, self.y = 220, 550
+        self.image = load_image('resource/skill/skill_earth.png')
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Skill__T:
+    def __init__(self):
+        self.x, self.y = 260, 550
+        self.image = load_image('resource/skill/skill_tree.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
