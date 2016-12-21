c7dd4469c7ddc20610e297666e61dcc27c98203a
leehyerin <lhr_id@naver.com>
2016-12-21 20:31:25 +0900

    new version

--- TIme.py
+++ TIme.py
@@ -4,9 +4,11 @@ from pico2d import *
 class Time:
     def __init__(self):
         self.font = load_font('ENCR10B.ttf',40)
+        self.init_time = SDL_GetTicks()
 
-    def update(self,frame_time):
-        self.time = get_time()
 
-    def draw(self):
-        self.font.draw(50,550,"Time : %f " % self.time)
+    def passed_time(self):
+        return (self.init_time-SDL_GetTicks())
+
+    def update(self):
+        print(self.passed_time())
