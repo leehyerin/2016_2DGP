68189113b7de9bd98e9cc8ac0ad5bf3b81ff6829
leehyerin <lhr_id@naver.com>
2016-12-22 09:11:43 +0900

    3 kind of skill ^0^

--- Skill.py
+++ Skill.py
@@ -6,7 +6,7 @@ from Pokemon import*
 class Skill_WideDamage_W:
 
     PIXEL_PER_METER = (10.0 / 0.25)  # 10pixel이 25cm임
-    RUN_SPEED_KMPH = 20.0
+    RUN_SPEED_KMPH = 10.0
     RUN_SPEED_MPM = (RUN_SPEED_KMPH * 200.0 / 60.0)
     RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
     RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -19,7 +19,6 @@ class Skill_WideDamage_W:
         self.time=Time()
         self.x,self.y = Pokemon.x,Pokemon.y
         self.frame = 0
-        self.dead = 10
         if Skill_WideDamage_W. image == None:
             self.image = load_image('resource/skill/wide_damage.png')
 
@@ -55,7 +54,6 @@ class Skill_WideDamage_W:
     def update(self,frame_time):
         self.distance = Projectile.RUN_SPEED_PPS * frame_time
         self.frame = (self.frame + 1) % 4
-        self.dead -=1
         self.handle_state[self.state](self)
 
         # if self.time.passed_time() > 300:
@@ -69,11 +67,73 @@ class Skill_WideDamage_W:
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def draw(self):
-        self.image.clip_draw(self.frame * 50, 0, 50, 99, self.x, self.y)
+        self.image.clip_draw(self.frame * 50, 0, 50, 99, self.x, self.y,100,200)
 
 #######################################################
 
+class Skill_Flame_F:
+    PIXEL_PER_METER = (10.0 / 0.25)  # 10pixel이 25cm임
+    RUN_SPEED_KMPH = 10.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 200.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
+    LEFT, RIGHT, UP, DOWN = 0, 1, 2, 3
+
+    image = None
+
+    def __init__(self, Pokemon):
+        self.time = Time()
+        self.x, self.y = Pokemon.x, Pokemon.y
+        self.frame = 0
+        if Skill_Flame_F.image == None:
+            self.image = load_image('resource/skill/flame.png')
+
+        if Pokemon.state in (Pokemon.LEFT_RUN, Pokemon.LEFT_IDLE):
+            self.state = self.LEFT
+        elif Pokemon.state in (Pokemon.RIGHT_RUN, Pokemon.RIGHT_IDLE):
+            self.state = self.RIGHT
+        elif Pokemon.state in (Pokemon.UP_RUN, Pokemon.UP_IDLE):
+            self.state = self.UP
+        elif Pokemon.state in (Pokemon.DOWN_RUN, Pokemon.DOWN_IDLE):
+            self.state = self.DOWN
+
+    def create_left(self):
+        self.x -= 7 * self.distance
+
+    def create_right(self):
+        self.x += 7 * self.distance
+
+    def create_up(self):
+        self.y += 7 * self.distance
+
+    def create_down(self):
+        self.y -= 7 * self.distance
+
+    handle_state = {
+        LEFT: create_left,
+        RIGHT: create_right,
+        UP: create_up,
+        DOWN: create_down
+
+    }
+
+    def update(self, frame_time):
+        self.distance = Projectile.RUN_SPEED_PPS * frame_time
+        self.frame = (self.frame + 1) % 4
+        self.handle_state[self.state](self)
+
+        # if self.time.passed_time() > 300:
+        #     Skill_WideDamage_W.remove()
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 34, 0, 34, 49, self.x, self.y)
 
 
 
@@ -97,3 +157,37 @@ class Skill_Button:
 
     def draw(self):
         self.image.draw(self.x, self.y)
+
+######################################################################################
+
+
+class Skill_Stem_Chain:
+
+    PIXEL_PER_METER = (10.0 / 0.25)  # 10pixel이 25cm임
+    RUN_SPEED_KMPH = 10.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 200.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    LEFT, RIGHT, UP, DOWN = 0, 1, 2, 3
+
+    image = None
+
+    def __init__(self, Pokemon: object) -> object:
+        self.time=Time()
+        self.x,self.y = Pokemon.x,Pokemon.y
+        self.frame = 0
+        if Skill_WideDamage_W. image == None:
+            self.image = load_image('resource/skill/stem_chain.png')
+
+
+    def update(self,frame_time):
+        self.distance = Projectile.RUN_SPEED_PPS * frame_time
+        self.frame = (self.frame + 1) % 4
+
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.clip_draw(0, self.frame * 50, 42, 50, self.x, self.y)
